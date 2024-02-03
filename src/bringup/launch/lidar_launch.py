85b45d9fb7718657b0899b0b7a90368b01dd461f
ikemura_kei <ikemurakei2001@gmail.com>
2024-02-03 22:51:03 +0100

    added lidar launch file as well as a launch file that launchs visualization for lidar too

--- src/bringup/launch/lidar_launch.py
+++ src/bringup/launch/lidar_launch.py
@@ -67,10 +67,6 @@ def generate_launch_description():
                          'angle_compensate': angle_compensate}],
             output='screen'),
 
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='base_link_to_laser',
-            parameters=["0", "0", "0", "0", "0", "0", "base_link", "laser"],
-            output='screen'),
+      Node(package='tf2_ros', executable='static_transform_publisher', exec_name='base_link_2_laser_tf', arguments=['--child-frame-id', 'laser', '--frame-id', 'base_link'])
+
     ])
--- src/bringup/launch/view_launch.py
+++ src/bringup/launch/view_launch.py
@@ -8,13 +8,20 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import LogInfo
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-rviz_path = "/home/team2/dd2419_ws/src/bringup/rviz/dd2419_basic.rviz"
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
 def generate_launch_description():    
+    bringup_dir = get_package_share_directory('bringup')
+    rviz_config = 'rviz/basic.rviz'
+    lidar_launch = 'launch/lidar_launch.py'
+    included_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(bringup_dir, lidar_launch)))
+    
+    rviz_config = os.path.join(bringup_dir, rviz_config)
+    print(rviz_config)
+    rviz_node = Node(executable='rviz2', package='rviz2', arguments=['-d', rviz_config])
     return LaunchDescription([
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            parameters=['-d', str(rviz_path)],
-            output='screen'),
+        included_launch,
+        rviz_node
     ])
