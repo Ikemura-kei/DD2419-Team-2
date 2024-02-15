35cc1bce27f068cc83758b726be579e0b71610c2
ikemura_kei <ikemurakei2001@gmail.com>
2024-02-15 12:10:34 +0100

    added map publishing

--- src/dd2419_simulation/setup.py
+++ src/dd2419_simulation/setup.py
@@ -25,6 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'simulation_node = dd2419_simulation.simulation_node:main',
         ],
     },
 )
