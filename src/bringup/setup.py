c2eb106cd95f4c71efeb37c51a4e2e9630d2f7ea
MaelArr <mael.arrive@insa-lyon.fr>
2024-02-21 21:26:32 +0100

    re-organized launch files in bringup a bit

--- src/bringup/setup.py
+++ src/bringup/setup.py
@@ -13,6 +13,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch/demo', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch/milestones', '*launch.[pxy][yma]*'))),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'configs'), glob('configs/*.yaml')),
     ],
