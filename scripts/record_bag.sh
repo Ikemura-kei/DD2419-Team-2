ab771475cc0e8d61eb1b6c0c42d80e0bc8d81074
MaelArr <mael.arrive@insa-lyon.fr>
2024-02-21 21:25:34 +0100

    edited topics we want to record

--- scripts/record_bag.sh
+++ scripts/record_bag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /home/team2/dd2419_ws/bags && ros2 bag record /tf_static /scan /motor/encoders /motor/duty_cycles /motor/current_duty_cycles
+cd /home/team2/dd2419_ws/bags && ros2 bag record /tf_static /scan /tf
